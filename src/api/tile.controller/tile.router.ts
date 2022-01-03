4ed684fd8c5f98497369dc2fa86d1c5cf8daf784
Liad <liadaltiff@gmail.com>
2022-01-03 13:57:28 +0200

    update

--- src/api/tile.controller/tile.router.ts
+++ src/api/tile.controller/tile.router.ts
@@ -8,4 +8,7 @@ tileRouter.route("/").get(getAllTiles).post(createTile);
 tileRouter.route("/state").post(sendTilesToServer);
 tileRouter.route("/").get(getAllTiles);
 // tileRouter.route("/:userId").get(getOneUser);
+// tileRouter.route("/:userId").get(getOneUser);
+// tileRouter.route("/:userId").get(getOneUser);
+
 export default tileRouter;
